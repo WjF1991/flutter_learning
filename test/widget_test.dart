1a7d50400bca23aa40ef58c824ce0eafd816d66e
wangjunfei <fengzhong41a@qq.com>
2023-04-01 00:13:47 +0800

    添加启动页

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sramoon/main.dart';
+import 'package:flutter_app_demo02/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
